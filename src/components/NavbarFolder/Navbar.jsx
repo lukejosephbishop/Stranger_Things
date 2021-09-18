4b0f4b544d5abed7aac006f85448bb0a5f0fc788
Luke <reboluketion42@gmail.com>
2021-09-17 17:19:00 -0700

    more css and layout changes

--- src/components/NavbarFolder/Navbar.jsx
+++ src/components/NavbarFolder/Navbar.jsx
@@ -14,25 +14,25 @@ const Navbar = () => {
           <i className="fas fa-space-shuttle"></i>
         </h3>
         <h3>
-        <i class="fas fa-couch"></i>
+          <i className="fas fa-couch"></i>
         </h3>
-<Link className="navbar-logo" to="/home">
-        <h1 >Stranger's Things</h1>
-</Link>
+        <Link className="navbar-logo" to="/home">
+          <h1>Stranger's Things</h1>
+        </Link>
       </nav>
       <div className="menu-icon">
-      <Link className="nav-links" to="/home">
-        Home
-      </Link>
-      <Link className="nav-links" to="/posts">
-        Posts
-      </Link>
-      <Link className="nav-links" to="/profile">
-        Profile
-      </Link>
-      <Link className="nav-links" to="/login">
-        Login
-      </Link>
+        <Link className="nav-links" to="/home">
+          Home
+        </Link>
+        <Link className="nav-links" to="/posts">
+          Posts
+        </Link>
+        <Link className="nav-links" to="/profile">
+          Profile
+        </Link>
+        <Link className="nav-links" to="/login">
+          Login
+        </Link>
       </div>
     </nav>
   );
