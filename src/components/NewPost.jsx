f7fc3295c7183df388b23b748c991b0fad5b5574
nedaraeva <nedaraeva@github.com>
2021-09-22 17:38:50 -0700

    useState changes on newPost

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewPost() {
+ 
+  const [title, setTitle]=useState("")
+  const [description, setDescription]=useState("")
+  const [price, setPrice]=useState("")
+  const [location, setLocation]=useState("")
+ 
   return (
     <form className="newpost">
       <h1 className="newpost-title"> Create New Post</h1>
@@ -8,13 +14,25 @@ function NewPost() {
       
       <div className="">
         <label htmlFor="title">Title</label>
-        <input type="text" name="title" placeholder="title" required></input>
+        <input type="text" name="title" placeholder="title" value={title}
+            onChange={(event) => {
+              setTitle(event.target.value)
+            }} required></input>
         <label htmlFor="description">Description</label>
-        <input type="text" name="description" placeholder="description" required></input>
+        <input type="text" name="description" placeholder="description" value={description}
+            onChange={(event) => {
+              setDescription(event.target.value)
+            }} required></input>
         <label htmlFor="price">Price</label>
-        <input type="text" name="price" placeholder="price" required></input>
+        <input type="text" name="price" placeholder="price" value={price}
+            onChange={(event) => {
+              setPrice(event.target.value)
+            }} required></input>
         <label htmlFor="location">Location</label>
-        <input type="text" name="location" placeholder="location" required></input>
+        <input type="text" name="location" placeholder="location" value={location}
+            onChange={(event) => {
+              setLocation(event.target.value)
+            }} required></input>
         <label className="checkbox">
             <input type="checkbox" name="delivery" /> Willing to Deliver
           </label>
